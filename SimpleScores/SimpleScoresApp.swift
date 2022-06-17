baeeb7d975dd80e34cc84fea906e7d99112cc60f
kashiash <kashiash@gmail.com>
2022-06-17 20:04:42 +0200

    Initial Commit

---
+++ SimpleScores/SimpleScoresApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleScoresApp.swift
+//  SimpleScores
+//
+//  Created by Jacek on 17/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct SimpleScoresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
