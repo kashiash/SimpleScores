f4ca9038cb3d0af37f4fe9feed8f42462a92da7e
kashiash <kashiash@gmail.com>
2022-06-17 20:11:03 +0200

    start

---
+++ SimpleScores/ColorChoice.swift
@@ -0,0 +1,12 @@
+//
+//  ColorChoice.swift
+//  SimpleScores
+//
+//  Created by Jacek on 17/06/2022.
+//
+
+import Foundation
+
+enum ColorChoice: String, Codable, CaseIterable {
+    case blue, green, orange, purple, red, teal, indigo, gray
+}
